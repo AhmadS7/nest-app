ac71424e302aae22a04387272d7a79f094a1bfc8
Ahmad <myvirtualrabbit15@gmail.com>
2021-08-28 14:11:04 +0600

    Final

---
+++ src/common/middlewares/validStudent.middleware.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prefer-const */
+/* eslint-disable prettier/prettier */
+
+import { Injectable, NestMiddleware, HttpException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { students } from '../../db';
+
+@Injectable()
+export class ValidStudentMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Hi! Middleware was called');
+    const studentId = req.params.studentId;
+    let studentExists = students.some((student) => {
+      return student.id === studentId;
+    });
+    if (!studentExists) {
+      throw new HttpException('Student not found', 400);
+    }
+    next();
+  }
+}
